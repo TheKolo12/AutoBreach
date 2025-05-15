1537827b6a0bda1fa5bfac9fdf6b5ff5a87712a8
Kolo <vacca1089@gmail.com>
2025-05-15 22:56:36 +0200

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -1,24 +1,25 @@
-﻿using Exiled.API.Enums;
+using Exiled.API.Enums;
 using Exiled.API.Interfaces;
 using PlayerRoles;
 using System.Collections.Generic;
 using System.ComponentModel;
-// :)
+
 namespace AutoBreach
 {
     public class Config : IConfig
     {
         [Description("Whether the plugin is enabled.")]
         public bool IsEnabled { get; set; } = true;
+
         public bool Debug { get; set; } = false;
 
-        [Description("Spawn locations with their respective chances. Format: Location: Chance")]
-        public static Dictionary<RoleTypeId, (string message, float duration)> CassieMessages { get; set; } = new Dictionary<RoleTypeId, (string, float)>
-        {       
-            { RoleTypeId.Scp096, ("SCP-096 Has broke the containment!", 10f ) },
-            { RoleTypeId.Scp173, ("SCP-173 Has broke the containment!", 10f ) },
-            { RoleTypeId.Scp049, ("SCP-049 Has broke the containment", 10f ) },
-            { RoleTypeId.Scp079, ("SCP-079 has breached the facility!", 10f ) },
+        [Description("Cassie for their respective SCP. Format: SCP, Cassie, Duration")]
+        public List<CassieMessage> CassieMessages { get; set; } = new()
+        {
+            new CassieMessage { Role = RoleTypeId.Scp096, Message = "SCP-096 Has broke the containment!", Duration = 10f },
+            new CassieMessage { Role = RoleTypeId.Scp173, Message = "SCP-173 Has broke the containment!", Duration = 10f },
+            new CassieMessage { Role = RoleTypeId.Scp049, Message = "SCP-049 Has broke the containment", Duration = 10f },
+            new CassieMessage { Role = RoleTypeId.Scp079, Message = "SCP-079 has breached the facility!", Duration = 10f },
         };
-    };
+    }
 }
