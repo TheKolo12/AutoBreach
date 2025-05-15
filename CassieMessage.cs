678ec7c02f5e161f2b7a2651f0760253955355a0
Kolo <vacca1089@gmail.com>
2025-05-15 22:55:57 +0200

    Add files via upload

---
+++ CassieMessage.cs
@@ -0,0 +1,11 @@
+﻿using PlayerRoles;
+
+namespace AutoBreach
+{
+    public class CassieMessage
+    {
+        public RoleTypeId Role { get; set; }
+        public string Message { get; set; }
+        public float Duration { get; set; }
+    }
+}
