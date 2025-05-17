bc97c02c20c709480abe2561ad8f522fa8531f58
Kolo <vacca1089@gmail.com>
2025-05-17 14:35:07 +0200

    Update Config.cs

--- Config.cs
+++ Config.cs
@@ -13,13 +13,13 @@ namespace AutoBreach
 
         public bool Debug { get; set; } = false;
 
-        [Description("Cassie for their respective SCP. Format: SCP, Cassie, Duration")]
+        [Description("Cassie for their respective SCP. Format: SCP, Cassie, Subtiles (On or Off)")]
         public List<CassieMessage> CassieMessages { get; set; } = new()
         {
-            new CassieMessage { Role = RoleTypeId.Scp096, Message = "SCP-096 Has broke the containment!", Duration = 10f },
-            new CassieMessage { Role = RoleTypeId.Scp173, Message = "SCP-173 Has broke the containment!", Duration = 10f },
-            new CassieMessage { Role = RoleTypeId.Scp049, Message = "SCP-049 Has broke the containment", Duration = 10f },
-            new CassieMessage { Role = RoleTypeId.Scp079, Message = "SCP-079 has breached the facility!", Duration = 10f },
+            new CassieMessage { Role = RoleTypeId.Scp096, Message = "SCP-096 Has broke the containment!", Subtiles = true },
+            new CassieMessage { Role = RoleTypeId.Scp173, Message = "SCP-173 Has broke the containment!", Subtiles = true },
+            new CassieMessage { Role = RoleTypeId.Scp049, Message = "SCP-049 Has broke the containment", Subtiles = true },
+            new CassieMessage { Role = RoleTypeId.Scp079, Message = "SCP-079 has breached the facility!", Subtiles = true },
         };
     }
 }
